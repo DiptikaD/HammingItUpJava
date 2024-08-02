a81e52aa6e4792b2f337d7d72ed0dfa523243862
Devi <diptikadevi@gmail.com>
2024-08-02 13:30:21 -0400

    finished!!

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,27 @@
-package zipcode;
 
 public class Hamming {
+
+    String sequence1 = "";
+    String sequence2 = "";
     public Hamming(String s, String s1) {
+        if (s.length() != s1.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        sequence1 = s;
+        sequence2 = s1;
     }
 
     public int getHammingDistance() {
-        return -1;
+
+        String[] sequence1arr = sequence1.split("");
+        String[] sequence2arr = sequence2.split("");
+        int hamCounter = 0;
+
+        for (int i = 0; i < sequence1.length(); i++){
+            if (!sequence1arr[i].matches(sequence2arr[i])){
+                hamCounter++;
+            }
+        }
+        return hamCounter;
     }
 }
